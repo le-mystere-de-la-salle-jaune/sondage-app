5b81eac851ea6fd4b3fc9033b5b4be513e3591d3
Josias NTUAREMBA <josiasdan@hotmail.fr>
2018-07-26 17:44:58 +0200

    Update liste-resultats.component.ts

--- src/app/liste-resultats/liste-resultats.component.ts
+++ src/app/liste-resultats/liste-resultats.component.ts
@@ -20,12 +20,15 @@ export class ListeResultatsComponent implements OnInit {
 
   constructor(private resultatSondageService:ResultatSondageService,private _route: ActivatedRoute,private router: Router) {
      this.idStagiaire = Number.parseInt(_route.snapshot.paramMap.get("id"))	     this.idStagiaire = Number.parseInt(_route.snapshot.paramMap.get("id"))
--    sondageservice.listerSondages(this.idStagiaire).then((sondages:any) => {	+    resultatSondageService.listerResultatSondageStagiaire(this.idStagiaire).then((resultatSondage:any) => {
--      sondages.forEach(sondage => {	+      resultatSondage.forEach(resultatsondage => {
--        this.listeSondages.push(sondage);	+        this.listeResultatSondage.push(resultatsondage);
-       });	       });
-     });	     });
-    }	    }
+     sondageservice.listerSondages(this.idStagiaire).then((sondages:any) => {	resultatSondageService.listerResultatSondageStagiaire(this.idStagiaire).then((resultatSondage:any) => {
+       sondages.forEach(sondage => {resultatSondage.forEach(resultatsondage => {
+         this.listeSondages.push(sondage);this.listeResultatSondage.push(resultatsondage);
+       });
+    });
+ });	     
+ });
+    }
+}
 
   ngOnInit() {
   }
