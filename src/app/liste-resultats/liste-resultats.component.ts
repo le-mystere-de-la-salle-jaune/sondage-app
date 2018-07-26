c94d6b64a000ad8b23581a9958246cea53824736
Josias NTUAREMBA <josiasdan@hotmail.fr>
2018-07-26 17:50:18 +0200

    Update liste-resultats.component.ts

--- src/app/liste-resultats/liste-resultats.component.ts
+++ src/app/liste-resultats/liste-resultats.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ResultatSondage } from '../domains';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Route } from '@angular/router';
 import { ResultatSondageService } from '../services/resultat-sondage.service';
-
 
 
 @Component({
@@ -14,26 +12,23 @@ import { ResultatSondageService } from '../services/resultat-sondage.service';
 })
 export class ListeResultatsComponent implements OnInit {
 
-
   listeResultatSondage:ResultatSondage[]=[]
   idStagiaire:number
 
   constructor(private resultatSondageService:ResultatSondageService,private _route: ActivatedRoute,private router: Router) {
-     this.idStagiaire = Number.parseInt(_route.snapshot.paramMap.get("id"))	     this.idStagiaire = Number.parseInt(_route.snapshot.paramMap.get("id"))
-     sondageservice.listerSondages(this.idStagiaire).then((sondages:any) => {	resultatSondageService.listerResultatSondageStagiaire(this.idStagiaire).then((resultatSondage:any) => {
-       sondages.forEach(sondage => {resultatSondage.forEach(resultatsondage => {
-         this.listeSondages.push(sondage);this.listeResultatSondage.push(resultatsondage);
-       });
+    this.idStagiaire = Number.parseInt(_route.snapshot.paramMap.get("id"))
+    resultatSondageService.listerResultatSondageStagiaire(this.idStagiaire).then((resultatSondage:any) => {
+      resultatSondage.forEach(resultatsondage => {
+        this.listeResultatSondage.push(resultatsondage);
+      });
     });
- });	     
- });
-    }
-}
+   }
 
   ngOnInit() {
   }
   
-  affichageResultatSondage(id:string){
-    this.router.navigate([`/${this.stagiaire.id}/resultats/${id}`])
+  afiichageResultatSondage(id:string){
+    let idNumeriser = Number.parseInt(id)
+    this.router.navigate([`/${this.idStagiaire}/resultats/${idNumeriser}`])
   }
 }
